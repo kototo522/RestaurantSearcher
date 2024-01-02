e5cbe8fb509dfdd69b97da1f0c67bf204a3956c4
kototo522 <huyejiben@gmail.com>
2024-01-03 02:54:47 +0900

    add: お気に入りボタンの追加

--- app/src/main/java/com/example/restaurantsearcher/ui/storeDetail/StoreDetailScreen.kt
+++ app/src/main/java/com/example/restaurantsearcher/ui/storeDetail/StoreDetailScreen.kt
@@ -1,12 +1,17 @@
 package com.example.restaurantsearcher.ui.storeDetail
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Place
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,7 +21,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter.Companion.tint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -28,6 +40,7 @@ import coil.compose.AsyncImage
 fun StoreDetailScreen(navController: NavController) {
     val storeDetailViewModel = StoreDetailViewModel()
     val item = storeDetailViewModel.sampleShop
+    var isStarred by remember { mutableStateOf(false) }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -56,12 +69,22 @@ fun StoreDetailScreen(navController: NavController) {
                                 .weight(1f)
                                 .padding(horizontal = 24.dp),
                     ) {
-                        Text(
-                            text = item.name,
-                            fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                            fontWeight = MaterialTheme.typography.titleLarge.fontWeight,
-                            modifier = Modifier.padding(vertical = 8.dp),
-                        )
+                        Row(
+                            modifier = Modifier.padding(8.dp).fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            Text(
+                                text = item.name,
+                                fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                                fontWeight = MaterialTheme.typography.titleLarge.fontWeight,
+                            )
+                            Icon(
+                                imageVector = Icons.Default.Star,
+                                contentDescription = "お気に入り",
+                                modifier = Modifier.size(32.dp).clickable { isStarred = !isStarred },
+                                tint = if (isStarred) Color.Yellow else Color.LightGray,
+                            )
+                        }
                         Row(modifier = Modifier.padding(bottom = 8.dp)) {
                             Icon(
                                 imageVector = Icons.Default.Place,
